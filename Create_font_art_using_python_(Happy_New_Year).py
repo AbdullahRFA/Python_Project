bfc8f620866be7874d410bb4f9d95ffc97fe014a
AbdullahRFA <shakibrybmn@gmail.com>
2025-01-01 10:17:12 +0600

    spelling correct of given function

--- Create_font_art_using_python_(Happy_New_Year).py
+++ Create_font_art_using_python_(Happy_New_Year).py
@@ -1,7 +1,7 @@
 from colorama import Fore, Style
 import pyfiglet
 
-def display_ascci_art(message, color):
+def display_ascii_art(message, color):
     """
     Function to render and display ASCII art in a specified color.
 
@@ -13,9 +13,9 @@ def display_ascci_art(message, color):
     print(color+ascii_art+Style.RESET_ALL)
 
 def main():
-     display_ascci_art("Happy New Year", Fore.GREEN)
-     display_ascci_art("Wish You All The Best", Fore.YELLOW)
-     display_ascci_art("Celebrate With Joy!", Fore.CYAN)
+     display_ascii_art("Happy New Year", Fore.GREEN)
+     display_ascii_art("Wish You All The Best", Fore.YELLOW)
+     display_ascii_art("Celebrate With Joy!", Fore.CYAN)
      
 if __name__ == "__main__":
     main()
